ea7f87ecab2e72dd692289a34796bc34ea00498f
Anwesha <anweshadas909@gmail.com>
2024-12-12 21:45:07 +0530

    commit 9: excel sheet

--- src/Pages/subjectPage.tsx
+++ src/Pages/subjectPage.tsx
@@ -3,13 +3,15 @@ import {useAppDispatch, useAppSelector} from "../redux/store.ts";
 import {getClassesThunk} from "../redux/classesSlice.ts";
 import {useParams} from "react-router-dom";
 import {getDate} from "../Util/Naming_Conv.ts";
-import _ from "lodash";
+import _ , {isArray} from "lodash";
 import {Class} from "../Model/classes.ts";
 import {ClassBlock} from "../Component/ClassBlock.tsx";
 import "react-datepicker/dist/react-datepicker.css";
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import { DatePicker } from 'rsuite';
 import {getSubjectByIdThunk} from "../redux/getSubjectById.ts";
+import {data} from "autoprefixer";
+import ExcelJS from "exceljs"
 
 export function SubjectPage() {
 
@@ -17,6 +19,8 @@ export function SubjectPage() {
     const [endDate, setEndDate] = useState<Date | null>(new Date());
     const params = useParams()
     const { classes } = useAppSelector(state => state.class)
+    const { subject } = useAppSelector(state => state.subjectById)
+
     let groupedClass
 
     const today = new Date()
@@ -31,16 +35,161 @@ export function SubjectPage() {
     console.log(JSON.stringify(groupedClass))
 
     useEffect ( () => {
-        dispatch(getSubjectByIdThunk({id : classes.subjectId}))
-    } , [classes] );
+        dispatch(getSubjectByIdThunk({id : params.id}))
+    } , [] );
 
 
     function handleExport(){
-        const csvData = [
-            ["Roll Number",[endDate-startDate].map(date=>date)]
-        ]
-        console.log(csvData)
+        // const formattedDate = (date) => new Intl.DateTimeFormat('en-US').format(date);
+        //         const diffTime = Math.abs(endDate-startDate)
+        //         const diffDay = Math.floor(diffTime/(1000 * 60 * 60 * 24))
+        //         // const dates = Math.floor(diffTime/(1000 * 60 * 60 * 24))
+        //         const diff = formattedDate(endDate) - formattedDate(startDate)
+
+        const diff = Math.ceil((endDate-startDate)/(1000 * 60 * 60 * 24))
+
+        console.log(diff)
+        const dateFormat  = {weekday:'short',month:'short',day:'numeric'}
+        const dates = Array.from(
+            {length: diff},
+            (_,i) => {
+                const date  = startDate
+                date?.setDate(startDate?.getDate()+1)
+                // const [weekdayStr, dateStr] = date.toLocaleDateString('en-US',dateFormat).split(', ')
+                // return new Intl.DateTimeFormat('en-US').format(date);
+                return date?.toDateString()
+            }
+        )
+
+        console.log(dates )
+
+
+        classes.map(classs=>console.log(new Date(classs.createdOn).toDateString()))
+
+
+
+        // const mockData = [
+        //     // subject?.studentsEnrolled
+        //     //     // .filter(student => dates.includes(classes)) // Ensure `dates` includes `classes`
+        //     //     .map(student => (
+        //     //         {
+        //     //             Roll_num: `${student}`, // Convert student to string
+        //     //             dates: `` // Add other necessary logic for `dates` if needed
+        //     //         }
+        //     //     )) || []
+        //         classes.map(one_class=>
+        //             dates.includes( new Date(one_class.createdOn).toDateString()) &&
+        //             {
+        //                 date : "p"
+        //             }
+        //         )
+        //     ]
+
+        let studentDates
+
+        studentDates = classes.filter(one_class=>
+            dates.includes(new Date(one_class.createdOn).toDateString())
+        )
+
+        const exportFile = () => {
+            const workbook = new ExcelJS.Workbook();
+            const sheet = workbook.addWorksheet("my sheet")
+            sheet.properties.defaultRowHeight = 50
+
+
+            sheet.columns = [
+                {
+                    header : "Roll Number",
+                    key : 'roll',
+                    width : 10
+                },
+            ...studentDates.map((date)=>(
+                    {
+                        header : new Intl.DateTimeFormat('en-US').format(date.createdOn)  ,
+                        key : new Intl.DateTimeFormat('en-US').format(date.createdOn) ,
+                        width : 10
+                    }
+                ))
+            ]
+
+            subject?.studentsEnrolled?.map(student=> {
+
+                const rowData = {roll : student}
+
+                studentDates?.map((date)=>{
+                    const isPresent = date.attendees?.includes(student)
+                    rowData[new Intl.DateTimeFormat('en-US').format(date.createdOn)] = isPresent ? "Present" : "Absent"
+                    console.log(student , rowData[new Intl.DateTimeFormat('en-US').format(date.createdOn)])
+                })
+
+                console.log(rowData)
+
+                sheet.addRow(rowData);
+
+                // sheet.addRow (
+                //     studentDates?.attendees?.includes(student) ?
+                //     {
+                //         roll : student ,
+                //         date : "Present"
+                //     }
+                //     : {
+                //         roll : student,
+                //         date : "Absent"
+                //     }
+                // )
+            })
+
+            const writeFile = (fileName, content) => {
+                const link = document.createElement("a");
+                const blob = new Blob([content], {
+                    type: "application/vnd.ms-excel;charset=utf-8;"
+                });
+                link.download = fileName;
+                link.href = URL.createObjectURL(blob);
+                link.click();
+            };
+
+            workbook.xlsx.writeBuffer().then(data=>{
+                writeFile("attendance_sheet",data)
+            })
+            .catch((error) => {
+                console.error("Error generating CSV:", error);
+            });
+
+
+        }
+
+
+            // console.log(studentDates)
+
+        exportFile()
+
+        // const mockData = (subject?.studentsEnrolled || []).map(student=>{
+        //
+        //
+        //     // studentDates =  classes.map(one_class=>
+        //     //      dates.filter(date => date?.includes(new Date(one_class.createdOn).toDateString()))
+        //     // )
+        //
+        //     studentDates.map(date=> {
+        //         return {
+        //             `roll_num` = student ,
+        //             `${studentDates[0]}` = 0
+        //         }
+        //     })
+        // })
+
+        // const mock = [
+        // dates.includes(classes) &&
+        // [
+        //     `${dates}` , "p"
+        // ]
+        //
+        // ]
+
     }
+
+
 
 
 
