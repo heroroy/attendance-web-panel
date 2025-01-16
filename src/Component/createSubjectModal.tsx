b3d88c4eb58ca94bf2f673fc09bdc6e2aa85e356
Anwesha <anweshadas909@gmail.com>
2025-01-16 14:03:18 +0530

    feat 19: overlap field error fixed

--- src/Component/createSubjectModal.tsx
+++ src/Component/createSubjectModal.tsx
@@ -1,5 +1,5 @@
 import {TextInput} from "./textInput.tsx";
-import {FormEvent  , useEffect , useState} from "react";
+import {FormEvent , useCallback , useEffect , useRef , useState} from "react";
 import {DropDown} from "./DropDown.tsx";
 import {useAppDispatch, useAppSelector} from "../redux/store.ts";
 import {subjectAddThunk} from "../redux/subjectSlice.ts";
@@ -20,6 +20,8 @@ type OnDismissProps = {
 }
 
 export function CreateSubjectModal({ onDismiss } : OnDismissProps) {
+
+    const modalRef = useRef(null)
 
     const [input , setInput] = useState<inputModal> ( {
         name : "",
@@ -59,6 +61,11 @@ export function CreateSubjectModal({ onDismiss } : OnDismissProps) {
         });
     }
     const handleFileUpload=(file : any)=>{
+        if(file.type !== "text/csv"){
+            alert("File must be Csv")
+            return
+        }
+
         setInput((prevState)=>({
             ...prevState,
             file : file
@@ -89,22 +96,17 @@ export function CreateSubjectModal({ onDismiss } : OnDismissProps) {
             // console.log(input.file)
             setInput ((prevState)=>( { ...prevState , name : input.name, file : input.file, sec : input.sec, department : input.department }) )
 
-            // setSubjectCard((prevState)=>[
-            //     ...prevState,
-            //     { dept_name : input.name , card : input }
-            // ])
-
             dispatch(subjectAddThunk({
-                creatorName : profile?.name,
-                department : input.department,
-                section : input.sec,
-                studentsEnrolled : roll.slice(0,roll.length-1),
-                title : input.name,
-                // id : input.name + "-" + input.department + "-" + input.sec,
-                id : uuidv4(),
-                createdBy : profile?.email?.split('@')[0],
-                created: new Date().getTime()
-            } as Subject ))
+                    creatorName : profile?.name,
+                    department : input.department,
+                    section : input.sec,
+                    studentsEnrolled : roll.slice(0,roll.length-1),
+                    title : input.name,
+                    // id : input.name + "-" + input.department + "-" + input.sec,
+                    id : uuidv4(),
+                    createdBy : profile?.email?.split('@')[0],
+                    created: new Date().getTime()
+                } as Subject ))
 
             onDismiss()
 
@@ -115,14 +117,29 @@ export function CreateSubjectModal({ onDismiss } : OnDismissProps) {
 
     // console.log(input)
     // console.log(roll)
+    // function handleClickOutside(e){
+    //     if(modalRef.current && !modalRef.current.contains(e.target)) onDismiss()
+    // }
+
+    useEffect(()=>{
+        if (modalRef.current) {
+            modalRef.current.showModal();
+        }
+        return () => {
+            if (modalRef.current) {
+                modalRef.current.close();
+            }
+        };
+    },[])
 
     return (
-        <div>
-            <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-                <div className="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
+        <div  className="fixed inset-0 z-10 flex items-center justify-center"
+              aria-labelledby="modal-title"
+              role="dialog"
+              aria-modal="true">
 
-                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            {/*backdrop*/}
+                <div className="fixed inset-0 bg-gray-500/75 transition-opacity opacity-5" onClick={onDismiss} aria-hidden="true"></div>
 
                         <div
                             className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all lg:w-auto sm:my-8 ">
@@ -141,10 +158,12 @@ export function CreateSubjectModal({ onDismiss } : OnDismissProps) {
                                                     id="name"
                                                     value={ input.name }
                                                     placeholder="Name"
-                                                    onChange={ (e) => setInput ( {
-                                                        ...input ,
-                                                        name : e.target.value
-                                                    } ) }
+                                                    onChange={ (e) => {
+                                                        setInput({
+                                                            ...input ,
+                                                            name : e.target.value
+                                                        } )
+                                                         }}
                                                     className={ `block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-700 focus:ring-2 focus:ring-inset focus:text-neutral-400 sm:text-sm/6 ` }
                                                     required={true}
                                                 />
@@ -167,8 +186,7 @@ export function CreateSubjectModal({ onDismiss } : OnDismissProps) {
                                                                 <span
                                                                     className="font-semibold">Click to upload</span> or
                                                                 drag and drop</p>
-                                                            <p className="text-xs text-gray-500 dark:text-gray-400">SVG,
-                                                                PNG, JPG or GIF (MAX. 800x400px)</p>
+                                                            <p className="text-xs text-gray-500 dark:text-gray-400">CSV file Only</p>
                                                         </div>
 
                                                         <TextInput
@@ -181,10 +199,6 @@ export function CreateSubjectModal({ onDismiss } : OnDismissProps) {
                                                             className="hidden"
                                                             required={true}
                                                         />
-
-                                                        {/*<input required={true}  id="dropzone-file" type="file" onChange={(e) => {*/}
-                                                        {/*    if (e.target.files) handleFileUpload(e.target.files[0]);*/}
-                                                        {/*}} className="hidden"/>*/}
 
                                                     </label>
                                                 </div>
@@ -223,10 +237,9 @@ export function CreateSubjectModal({ onDismiss } : OnDismissProps) {
                                 </button>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
         </div>
+
     );
 }
+
 
--- src/Component/textInput.tsx
+++ src/Component/textInput.tsx
@@ -10,14 +10,18 @@ interface TextInputProps {
 }
 
 export function TextInput({ name , id , value , error , onChange , ...props }: TextInputProps) {
+
     return (
-        <input
-            {...props}
-            id={ id }
-            name={ name }
-            value={ value }
-            onChange={ onChange }
-        />
+        <>
+            <input
+                {...props}
+                id={ id }
+                name={ name }
+                value={ value }
+                onChange={ onChange }
+            />
+            <p>{error}</p>
+        </>
     );
 }
 
