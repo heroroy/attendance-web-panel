251bf4ab946321d48e1a216836f1075185ba16bf
Anwesha <anweshadas909@gmail.com>
2025-01-16 13:50:43 +0530

    feat 19: production error fix

--- src/Component/DropDown.tsx
+++ src/Component/DropDown.tsx
@@ -1,5 +1,5 @@
 import {MdArrowDropDown} from "react-icons/md";
-import {Dispatch , SetStateAction , useState} from "react";
+import {Dispatch , SetStateAction , useRef , useState} from "react";
 import {inputModal} from "./createSubjectModal.tsx";
 
 
@@ -14,25 +14,32 @@ type DropDownProps = {
 export function DropDown({ items , title, setInput, input} : DropDownProps) {
 
     const [value , setValue ] = useState(title)
+    const detailsRef = useRef<HTMLDetailsElement | null>(null);
 
-    function handleClick(e : any ){
+    function handleClick(data : string, e : any ){
 
-        console.log(typeof e)
         e.preventDefault()
         // console.log(e.target.textContent)
         if(title === "Section"){
             setInput ( {
                 ...input ,
-                sec : e.target.textContent
+                // sec : e.target.textContent
+                sec : data
             } )
-            setValue(e.target.textContent)
+            // setValue(e.target.textContent)
+            setValue(data)
         }
         if(title === "Department"){
             setInput ( {
                 ...input ,
-                department : e.target.textContent
+                // department : e.target.textContent
+                department : data
             } )
-            setValue(e.target.textContent)
+            // setValue(e.target.textContent)
+            setValue(data)
+        }
+        if (detailsRef.current) {
+            detailsRef.current.open = false;
         }
     }
 
@@ -40,14 +47,14 @@ export function DropDown({ items , title, setInput, input} : DropDownProps) {
 
     return (
         <>
-            <details className="dropdown">
+            <details ref={detailsRef} className="dropdown details-modal">
                 <summary  className="btn m-1 inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-5 cursor-pointer collapse">
                     { value }
                     <MdArrowDropDown size={22}/>
                 </summary>
-                <ul onClick={handleClick} className="menu dropdown-content bg-base-100 cursor-pointer rounded-box z-[1] w-52 p-2 shadow ">
+                <ul  className="menu dropdown-content bg-base-100 cursor-pointer rounded-box z-[1] w-52 p-2 shadow ">
                     {items.map(data=>(
-                        <li><a style={{textDecoration : "none"}}>{data}</a></li>
+                        <li onClick={(event)=>handleClick(data, event)}><a  style={{textDecoration : "none"}}>{data}</a></li>
                     ))}
                 </ul>
             </details>
