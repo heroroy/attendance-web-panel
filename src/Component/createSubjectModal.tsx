3e8a1cd4b50d701e27f1bfd8cf198520d4eba8d0
Vaibhav Jaiswal <vaibhav.jaiswal3011@gmail.com>
2025-02-01 14:04:44 +0530

    fix: Outside Click

--- src/Component/createSubjectModal.tsx
+++ src/Component/createSubjectModal.tsx
@@ -88,11 +88,9 @@ export function CreateSubjectModal({onDismiss}: OnDismissProps) {
              aria-modal="true">
 
             {/*backdrop*/}
-            <div className="fixed inset-0 bg-gray-500/75 transition-opacity opacity-5" onClick={onDismiss}
-                 aria-hidden="true"></div>
+            <div className="fixed inset-0 bg-black transition-opacity opacity-40" onClick={onDismiss} aria-hidden="true"></div>
 
-            <div
-                className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all lg:w-auto sm:my-8 ">
+            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all lg:w-auto sm:my-8 ">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 ">
                     <div className="sm:flex sm:items-start ">
                         <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left flex gap-7">
